158bba768c52995fcd8192efd8a93ca8c76e9659
alex-jin45 <zhengzhengjin999@gmail.com>
2023-02-14 21:12:16 -0800

    update formatProbabilityFactors.js to deisplay totals and factors only

--- helpers/formatProbabilityFactors.js
+++ helpers/formatProbabilityFactors.js
@@ -1,34 +1,36 @@
 export const formatProbabilityFactors = (factIncWin, factDecWin) => {
   let formattedProbabilityFactors = [
     {
-      weight_value: "Factors Scores Influencing Win"
+      weight_value: "Factors"
       // "product": 1,
     },
     {
-      weight_value: "Total Factor Score"
+      weight_value: "Total"
       // "Total": 17,
     }
   ];
 
   let totalWeight = 0;
-  for (let i = 0; i < factIncWin.length; i++) {
-    const name = factIncWin[i].name;
-    const weight = factIncWin[i].weight.value;
-    console.log(formattedProbabilityFactors[0]);
-    console.log(" | name " + name + " | weight " + weight);
-    let source = {};
-    source[name] = weight;
-    Object.assign(formattedProbabilityFactors[0], source);
-    totalWeight = totalWeight + weight;
+  if (factIncWin) {
+    for (let i = 0; i < factIncWin.length; i++) {
+      const name = factIncWin[i].name;
+      const weight = factIncWin[i].weight.value;
+      let source = {};
+      source[name] = weight;
+      Object.assign(formattedProbabilityFactors[0], source);
+      totalWeight = totalWeight + weight;
+    }
   }
 
-  for (let i = 0; i < factDecWin.length; i++) {
-    const name = factDecWin[i].name;
-    const weight = factDecWin[i].weight.value;
-    let source = {};
-    source[name] = weight;
-    Object.assign(formattedProbabilityFactors[0], source);
-    totalWeight = totalWeight + weight;
+  if (factDecWin) {
+    for (let i = 0; i < factDecWin.length; i++) {
+      const name = factDecWin[i].name;
+      const weight = factDecWin[i].weight.value;
+      let source = {};
+      source[name] = weight;
+      Object.assign(formattedProbabilityFactors[0], source);
+      totalWeight = totalWeight + weight;
+    }
   }
 
   Object.assign(formattedProbabilityFactors[1], { Total: totalWeight });
