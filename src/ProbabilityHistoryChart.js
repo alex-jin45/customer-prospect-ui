941e8bc0068bcbedef049887f7eab10b28887141
alex-jin45 <zhengzhengjin999@gmail.com>
2023-02-14 21:15:14 -0800

    update ProabilityHistoryChart react component nivo chart to fit UI/UX

--- src/ProbabilityHistoryChart.js
+++ src/ProbabilityHistoryChart.js
@@ -1,15 +1,43 @@
 import React from "react";
 import { useTheme } from "@mui/material";
 import "./styles.css";
+import { tokens } from "./theme";
 
 import { ResponsiveLine } from "@nivo/line";
 
 const ProbabilityHistoryChart = (props) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <div className="chart">
       <ResponsiveLine
         data={props.data}
-        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        theme={{
+          textColor: colors.grey[100],
+          axis: {
+            domain: {
+              line: {
+                stroke: colors.grey[100]
+              }
+            },
+            ticks: {
+              line: {
+                stroke: colors.grey[100],
+                strokeWidth: 1
+              }
+            },
+            text: {
+              fill: colors.grey[100]
+            }
+          },
+          legends: {
+            text: {
+              fill: colors.grey[100]
+            }
+          }
+        }}
+        margin={{ top: 10, right: 160, bottom: 50, left: 50 }}
         xScale={{ type: "point" }}
         yScale={{
           type: "linear",
@@ -27,43 +55,40 @@ const ProbabilityHistoryChart = (props) => {
           tickSize: 5,
           tickPadding: 5,
           tickRotation: 0,
-          legend: "Days Ago",
+          legend: "Time Elapsed [Days]",
           legendOffset: 36,
           legendPosition: "middle"
         }}
         axisLeft={{
           orient: "left",
           tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
+          tickPadding: 0,
+          tickRotation: 1,
           legend: "Probability",
           legendOffset: -40,
           legendPosition: "middle"
         }}
         enableGridX={false}
-        colors={{ scheme: "nivo" }}
-        lineWidth={5}
         pointSize={10}
         pointColor={{ theme: "background" }}
         pointBorderWidth={2}
         pointBorderColor={{ from: "serieColor" }}
         pointLabelYOffset={-12}
-        areaOpacity={0.15}
         useMesh={true}
         legends={[
           {
             anchor: "bottom-right",
             direction: "column",
             justify: false,
-            translateX: 100,
+            translateX: 85,
             translateY: 0,
             itemsSpacing: 0,
             itemDirection: "left-to-right",
             itemWidth: 80,
             itemHeight: 20,
-            itemOpacity: 0.75,
-            symbolSize: 12,
-            symbolShape: "circle",
+            itemOpacity: 0.95,
+            symbolSize: 20,
+            symbolShape: "square",
             symbolBorderColor: "rgba(0, 0, 0, .5)",
             effects: [
               {
