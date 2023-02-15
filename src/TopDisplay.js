89aca3bdb55d6b1dc15b8fb05a5ba8efee7572ad
alex-jin45 <zhengzhengjin999@gmail.com>
2023-02-14 21:16:55 -0800

    add dark mode an dlight mode to TopDisplay for UI/UX

---
+++ src/TopDisplay.js
@@ -0,0 +1,29 @@
+import IconButton from "@mui/material/IconButton";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import { ColorModeContext, tokens } from "./theme";
+import { useContext } from "react";
+import { Box, useTheme } from "@mui/material";
+
+const TopDisplay = () => {
+  const theme = useTheme();
+  //const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      <Box>
+        {" "}
+        <h2>PILYTIX Scored Opportunities</h2>{" "}
+      </Box>
+      <IconButton onClick={colorMode.toggleColorMode}>
+        {theme.palette.mode === "dark" ? (
+          <DarkModeOutlinedIcon />
+        ) : (
+          <LightModeOutlinedIcon />
+        )}
+      </IconButton>
+    </Box>
+  );
+};
+
+export default TopDisplay;
