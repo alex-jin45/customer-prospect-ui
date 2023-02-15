be69c122da0a372e8e77ea2152ff65374972d290
alex-jin45 <zhengzhengjin999@gmail.com>
2023-02-15 15:22:35 -0800

    change parameter variable names to be consistant with all formating functions

--- src/helpers/formatProbabilityHistory.js
+++ src/helpers/formatProbabilityHistory.js
@@ -1,7 +1,7 @@
 const formatProbabilityHistory = (
   unformattedProbHist,
-  pilyProbNow,
-  repProbNow
+  currentPxProb,
+  currentRepProb
 ) => {
   let formattedProbabilityHistory = [
     {
@@ -16,6 +16,7 @@ const formatProbabilityHistory = (
     }
   ];
 
+  if(!unformattedProbHist) return formattedProbabilityHistory
   for (let index = 0; index < unformattedProbHist.length; index++) {
     const daysAgo = unformattedProbHist[index].daysAgo;
     let pilytixProb = unformattedProbHist[index].pilytixProb;
@@ -23,8 +24,8 @@ const formatProbabilityHistory = (
     formattedProbabilityHistory[0].data.push({ x: daysAgo, y: pilytixProb });
     formattedProbabilityHistory[1].data.push({ x: daysAgo, y: repProb });
   }
-  formattedProbabilityHistory[0].data.push({ x: 0, y: pilyProbNow });
-  formattedProbabilityHistory[1].data.push({ x: 0, y: repProbNow });
+  formattedProbabilityHistory[0].data.push({ x: 0, y: currentPxProb });
+  formattedProbabilityHistory[1].data.push({ x: 0, y: currentRepProb });
 
   //console.log(formattedProbabilityHistory);
   return formattedProbabilityHistory;
