252c6c1cdff7b88a2e6849201ddca42fd1230d4f
alex-jin45 <zhengzhengjin999@gmail.com>
2023-02-16 17:23:40 -0800

    attempt to color gradient manualley

--- src/helpers/formatProbabilityFactors.js
+++ src/helpers/formatProbabilityFactors.js
@@ -2,11 +2,11 @@ export const formatProbabilityFactors = (factsIncWin, factsDecWin) => {
   let formattedProbFacts = [
     [
       {
-        weight_value: "Factors"
+        id: "Factors"
         // "product": 1,
       },
       {
-        weight_value: "Total"
+        id: "Total"
         // "Total": 17,
       }
     ],
@@ -15,12 +15,40 @@ export const formatProbabilityFactors = (factsIncWin, factsDecWin) => {
     ]
   ];
 
+  const greenAccent = {
+    0: "#DCF5EF",
+    100: "#CCF1E7",
+    200: "#ACE8D9",
+    300: "#8CE0CA",
+    400: "#6CD7BB",
+    500: "#4CCEAC",
+    600: "#31B190",
+    700: "#24856C",
+    800: "#185948",
+    900: "#0C2D25"
+  };
+  const redAccent = {
+    0: "#F9E4E3",
+    100: "#F6D3D2",
+    200: "#EFB2B0",
+    300: "#E9918E",
+    400: "#E2706C",
+    500: "#DB4F4A",
+    600: "#C62D27",
+    700: "#97221E",
+    800: "#681815",
+    900: "#390D0B"
+  };
+
   let totalWeight = 0;
   if (factsIncWin) {
     for (let i = 0; i < factsIncWin.length; i++) {
       const name = factsIncWin[i].name;
       const weight = factsIncWin[i].weight.value; // let source = {};// source[name] = weight;
       Object.assign(formattedProbFacts[0][0], { [name]: weight });
+      Object.assign(formattedProbFacts[0][0], {
+        [name + "Color"]: greenAccent[i * 100]
+      });
 
       const message = factsIncWin[i].message;
       const description = factsIncWin[i].weight.description;
@@ -38,6 +66,9 @@ export const formatProbabilityFactors = (factsIncWin, factsDecWin) => {
       const name = factsDecWin[i].name;
       const weight = factsDecWin[i].weight.value; //let source = {};//source[name] = weight;
       Object.assign(formattedProbFacts[0][0], { [name]: weight });
+      Object.assign(formattedProbFacts[0][0], {
+        [name + "Color"]: redAccent[i * 100]
+      });
 
       const message = factsDecWin[i].message;
       const description = factsDecWin[i].weight.description;
@@ -55,8 +86,8 @@ export const formatProbabilityFactors = (factsIncWin, factsDecWin) => {
     Total: { message: "", description: "" }
   });
 
-  // console.log("all my formatted factors ");
-  // console.log(formattedProbFacts);
+  console.log("all my formatted factors ");
+  console.log(formattedProbFacts);
   return formattedProbFacts;
 };
 
