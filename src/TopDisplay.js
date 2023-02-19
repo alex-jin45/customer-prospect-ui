d36cbfba85a6d1a3833d82b99acf3ef8265a4153
alex-jin45 <zhengzhengjin999@gmail.com>
2023-02-19 15:43:47 -0800

    update TopDisplay.js react componenet to utilize Grid component from material UI and add the light dark theme Icon in the consistant location for the app

--- src/TopDisplay.js
+++ src/TopDisplay.js
@@ -3,26 +3,35 @@ import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import { ColorModeContext, tokens } from "./theme";
 import { useContext } from "react";
-import { Box, useTheme } from "@mui/material";
+import { Box, useTheme, Grid } from "@mui/material";
 
 const TopDisplay = () => {
   const theme = useTheme();
   //const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
   return (
-    <Box display="flex" justifyContent="space-between" p={2}>
-      <Box>
-        {" "}
-        <h2>PILYTIX Scored Opportunities</h2>{" "}
-      </Box>
-      <IconButton onClick={colorMode.toggleColorMode}>
-        {theme.palette.mode === "dark" ? (
-          <DarkModeOutlinedIcon />
-        ) : (
-          <LightModeOutlinedIcon />
-        )}
-      </IconButton>
-    </Box>
+    <Grid container spacing={0}>
+      <Grid item xs={11}>
+        <Box
+          ml={2}
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          flexWrap="wrap"
+        >
+          <h2>PILYTIX Scored Opportunities</h2>
+        </Box>
+      </Grid>
+      <Grid item xs={1} mt={2}>
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+      </Grid>
+    </Grid>
   );
 };
 
