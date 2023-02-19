f124d6924b1278ce5317772b3420fe1c7f603bfc
alex-jin45 <zhengzhengjin999@gmail.com>
2023-02-19 15:45:31 -0800

    update the them.js to add extra colors to red and green for nivo chart and to make sure that super dark colors aren't added to the nivo chart and make it too hard to read

--- src/theme.js
+++ src/theme.js
@@ -7,7 +7,7 @@ export const tokens = (mode) => ({
     ? {
         grey: {
           DEFAULT: "#666666",
-          50: "#C2C2C2",
+          0: "#C2C2C2",
           100: "#B8B8B8",
           200: "#A3A3A3",
           300: "#8F8F8F",
@@ -20,7 +20,7 @@ export const tokens = (mode) => ({
         },
         primary: {
           DEFAULT: "#141B2D",
-          50: "#4C67AC",
+          0: "#4C67AC",
           100: "#465F9E",
           200: "#3A4E82",
           300: "#2D3D65",
@@ -33,7 +33,7 @@ export const tokens = (mode) => ({
         },
         greenAccent: {
           DEFAULT: "#4CCEAC",
-          50: "#DCF5EF",
+          0: "#DCF5EF",
           100: "#CCF1E7",
           200: "#ACE8D9",
           300: "#8CE0CA",
@@ -42,11 +42,14 @@ export const tokens = (mode) => ({
           600: "#31B190",
           700: "#24856C",
           800: "#185948",
-          900: "#0C2D25"
+          900: "#185948",
+          1000: "#185948",
+          1100: "#185948",
+          1200: "#185948"
         },
         redAccent: {
           DEFAULT: "#DB4F4A",
-          50: "#F9E4E3",
+          0: "#F9E4E3",
           100: "#F6D3D2",
           200: "#EFB2B0",
           300: "#E9918E",
@@ -55,11 +58,14 @@ export const tokens = (mode) => ({
           600: "#C62D27",
           700: "#97221E",
           800: "#681815",
-          900: "#390D0B"
+          900: "#681815",
+          1000: "#681815",
+          1100: "#681815",
+          1200: "#681815"
         },
         blueAccent: {
           DEFAULT: "#6870FA",
-          50: "#FFFFFF",
+          0: "#FFFFFF",
           100: "#FFFFFF",
           200: "#DEE0FE",
           300: "#B7BBFD",
@@ -74,7 +80,7 @@ export const tokens = (mode) => ({
     : {
         grey: {
           DEFAULT: "#666666",
-          50: "#000000",
+          0: "#000000",
           100: "#121212",
           200: "#2E2E2E",
           300: "#4A4A4A",
@@ -87,7 +93,7 @@ export const tokens = (mode) => ({
         },
         primary: {
           DEFAULT: "#141B2D",
-          50: "#000000",
+          0: "#000000",
           100: "#000000",
           200: "#000000",
           300: "#030406",
@@ -100,7 +106,7 @@ export const tokens = (mode) => ({
         },
         greenAccent: {
           DEFAULT: "#4CCEAC",
-          50: "#0C2D25",
+          0: "#185948",
           100: "#185948",
           200: "#24856C",
           300: "#31B190",
@@ -113,7 +119,7 @@ export const tokens = (mode) => ({
         },
         redAccent: {
           DEFAULT: "#DB4F4A",
-          50: "#390D0B",
+          0: "#681815",
           100: "#681815",
           200: "#97221E",
           300: "#C62D27",
@@ -126,7 +132,7 @@ export const tokens = (mode) => ({
         },
         blueAccent: {
           DEFAULT: "#6870FA",
-          50: "#040B7D",
+          0: "#040B7D",
           100: "#060FB4",
           200: "#0814EA",
           300: "#323DF8",
