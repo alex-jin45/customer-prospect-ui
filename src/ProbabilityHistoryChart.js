7dea96ffe68d89147e8869188550ab531e4a9c47
alex-jin45 <zhengzhengjin999@gmail.com>
2023-02-19 15:41:27 -0800

    update ProbabilityHistoryChart.js componenent in order to reduce the amojnt of space on screen for mobile devices and add a hover effect to show more data

--- src/ProbabilityHistoryChart.js
+++ src/ProbabilityHistoryChart.js
@@ -9,10 +9,12 @@ const ProbabilityHistoryChart = (props) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
+  const data = props.data;
+
   return (
-    <div className="chart">
+    <div className="chartHistory">
       <ResponsiveLine
-        data={props.data}
+        data={data}
         theme={{
           textColor: colors.grey[100],
           axis: {
@@ -37,17 +39,17 @@ const ProbabilityHistoryChart = (props) => {
             }
           }
         }}
-        margin={{ top: 10, right: 160, bottom: 50, left: 50 }}
+        margin={{ top: 12, right: 20, bottom: 60, left: 50 }}
         xScale={{ type: "point" }}
         yScale={{
           type: "linear",
           min: "auto",
           max: "auto",
-          stacked: true,
+          stacked: false,
           reverse: false
         }}
         yFormat=" >-.2f"
-        curve="catmullRom"
+        curve="monotoneX"
         axisTop={null}
         axisRight={null}
         axisBottom={{
@@ -55,11 +57,12 @@ const ProbabilityHistoryChart = (props) => {
           tickSize: 5,
           tickPadding: 5,
           tickRotation: 0,
-          legend: "Time Elapsed [Days]",
+          legend: "Days Ago",
           legendOffset: 36,
           legendPosition: "middle"
         }}
         axisLeft={{
+          tickValues: 5,
           orient: "left",
           tickSize: 5,
           tickPadding: 0,
@@ -75,21 +78,54 @@ const ProbabilityHistoryChart = (props) => {
         pointBorderColor={{ from: "serieColor" }}
         pointLabelYOffset={-12}
         useMesh={true}
+        enableSlices="x"
+        sliceTooltip={({ slice }) => {
+          return (
+            <div
+              style={{
+                background: "#222222",
+                padding: "9px 12px",
+                textAlign: "left",
+                width: "120px",
+                borderRadius: "10px"
+              }}
+            >
+              {slice.points.map((point) => (
+                <div
+                  key={point.id}
+                  style={{
+                    color: point.serieColor,
+                    padding: "3px 0"
+                  }}
+                >
+                  <div>
+                    <strong>{point.data.xFormatted}</strong> Days Ago
+                  </div>
+                  <div>
+                    <strong> {point.data.yFormatted.replace(/^0+/, "")}</strong>{" "}
+                    Probability{" "}
+                  </div>
+                </div>
+              ))}
+            </div>
+          );
+        }}
         legends={[
           {
             anchor: "bottom-right",
-            direction: "column",
+            direction: "row",
             justify: false,
-            translateX: 85,
-            translateY: 0,
-            itemsSpacing: 0,
+            translateX: 0,
+            translateY: 51,
+            itemsSpacing: 7,
             itemDirection: "left-to-right",
-            itemWidth: 80,
-            itemHeight: 20,
-            itemOpacity: 0.95,
+            itemWidth: 45,
+            itemHeight: 30,
+            itemOpacity: 1,
             symbolSize: 20,
-            symbolShape: "square",
+            symbolShape: "circle",
             symbolBorderColor: "rgba(0, 0, 0, .5)",
+
             effects: [
               {
                 on: "hover",
