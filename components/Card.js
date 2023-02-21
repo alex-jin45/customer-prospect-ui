63853f16217d50e103faff486e51fa6900ef966f
alex-jin45 <zhengzhengjin999@gmail.com>
2023-02-21 10:05:57 -0800

    migrate react to next.js by importing all global css into the _app.js file in the pages folder per Next js guidlines

--- components/Card.js
+++ components/Card.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 import ProbabilityHistoryChart from "./ProbabilityHistoryChart";
+import ProbabilityFactorsTrendChart from "./ProbabilityFactorsTrendChart";
+
 
 import formatProbabilityHistory from "../helpers/formatProbabilityHistory";
 import formatProbabilityFactors from "../helpers/formatProbabilityFactors";
-import ProbabilityFactorsTrendChart from "./ProbabilityFactorsTrendChart";
 
 import { Box, Grid, IconButton } from "@mui/material";
 import CancelPresentationIcon from "@mui/icons-material/CancelPresentation";
@@ -37,6 +38,7 @@ const Card = (props) => {
             justifyContent="space-between"
             flexWrap="wrap"
           >
+            
             <table>
               <tr>
                 <td align="left" width="140px">
